b75be0baf38f55a85863fd1b62af168b32dc09f3
nexcer-it <nexcer-it@timwi.com>
2018-06-20 17:56:54 +0200

    New server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var server = express();
-server.set('port', (process.env.PORT || 8080));
-server.use('/', express.static(__dirname + '/www'));
-server.listen(server.get('port'), function () {
+var port = (process.env.PORT || 8080)
+server.set('port', port);
+server.use(express.static(__dirname + '/www'));
+server.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/www/index.html'));
+});
+server.listen(port, function () {
     console.log('Listening on port' + server.get('port'));
 });
