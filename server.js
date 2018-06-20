452cadb274e8237229c56038d20bdf2d405241d8
nexcer-it <nexcer-it@timwi.com>
2018-06-20 14:43:34 +0200

    Start node server with ENV port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var server = express();
+server.set('port', (process.env.PORT || 8080));
 server.use('/', express.static(__dirname + '/www'));
-server.listen(80);
+server.listen(server.get('port'));
